67823bbb0b68f86eab3a968ee958866db6a82ba5
有希奈 澤田 <150584@jc-21.jp>
2017-12-13 12:32:45 +0900

    強制終了やめてくれ

--- FoodStock01/FoodStock01/App.xaml.cs
+++ FoodStock01/FoodStock01/App.xaml.cs
@@ -14,17 +14,31 @@ namespace FoodStock01
         {
 
             // TabbedPageをMainPageとしてセットする
-            MainPage = new NavigationPage(new MainPage())
-            {
-                /*Children = {
+            MainPage = new NavigationPage(new MainPage());
+            /*{
+                Children = {
                    new FoodPage1("食材"),
                    new StockPage("保存"),
                    new EntryPage1("登録"),
                    new MemoPage1("メモ"),
                    new SettingPage("設定"),
                    new RecipePage("レシピ")
-                }*/
-            };
+                }
+            };*/
+        }
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
         }
     }
 }
